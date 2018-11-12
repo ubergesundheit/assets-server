ee7195399052ba0136e0b2ef1c6fd34b5576ee09
geraldpape <g.pape@foodtracks.de>
2018-11-12 11:18:33 +0100

    set cache control to 31536000 seconds

--- cmd/as-builder/templates.go
+++ cmd/as-builder/templates.go
@@ -217,7 +217,7 @@ func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if strings.HasSuffix(fpath, ".html") {
 		w.Header().Set("Cache-Control", "public, max-age=0, must-revalidate")
 	} else {
-		w.Header().Set("Cache-Control", "public, max-age=86400")
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
 	}
 	http.ServeContent(w, r, fpath, info.ModTime(), file)
 }
